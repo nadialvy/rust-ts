3323ead8cfbff5436d5bdbc9dd927f720889c439
nadialvy <5026231007@student.its.ac.id>
2024-04-19 12:02:07 +0700

    iterator

---
+++ src/index.ts
@@ -0,0 +1,3 @@
+const smt = [1,2,3].map((x) => x + 1);
+
+console.log(smt)
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
 fn main() {
-    println!("Hello, world!");
+    let foo: Vec<_> = vec![1, 2, 3] //bikin 3 belt
+        .iter() //hire pegawai untuk meletakkan setiap belt di conveyor
+        .map(|x| x + 1) //beli mesin untuk mengubah belt satu persatu
+        .collect(); //kumpulkan semua belt menjadi 1 container
+
+    // kenapa butuh collect. kalau berhenti di map doang, itu bentuknya masih iter dan bukan vector dan bukan list
+    // bentuknya masih iterator
+    // collect digunakan untuk mengubah dari iterator menjadi vector
+
+    // intinya = mengembalikan bentuk dari iterator menjadi sesuatu (di case ini vector)
+
+    println!("{:?}", foo);
 }
